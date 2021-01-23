e44ec34a637e3475c4402a883c9db11a9b87c8ac
HarperJo12 <68649970+HarperJo12@users.noreply.github.com>
2021-01-23 12:38:03 +0700

    Add files via upload
    just basic python exercise

---
+++ guiExercise.py
@@ -0,0 +1,26 @@
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+# for i, char in enumerate(picture):
+#   for y, char in enumerate(picture[i]):
+#     if picture[i][y] == 0:
+#       print(' ', end='')
+#     else:
+#       print('*', end='')
+#   print()
+
+for rows in picture:
+  for pixel in rows:
+    if pixel:
+      print(' ', end='')
+    else:
+      print('*', end='')
+  print()
